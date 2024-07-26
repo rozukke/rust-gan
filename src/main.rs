76aa348855d56f88da77c1bee0616db18114aa94
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-07-26 15:58:01 +1000

    Update main to run inference

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList};
+use std::fs;
+use std::path::Path;
 
 fn main() -> PyResult<()> {
-    Python::with_gil(|py| {
-        PyModule::from_code_bound(
-            py,
-            r#"def hello():
-                print("Hello from Python")
-            "#,
-            "",
-            "",
-        )?
-        .getattr("hello")?
-        .call0()?;
-        Ok(())
-    })
+    let path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/pysrc"));
+    let py_app = fs::read_to_string(path.join("main.py"))?;
+    let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+        let syspath = py
+            .import_bound("sys")?
+            .getattr("path")?
+            .downcast_into::<PyList>()?;
+        syspath.insert(0, path)?;
+        let app: Py<PyAny> = PyModule::from_code_bound(py, &py_app, "main.py", "main")?
+            .getattr("main")?
+            .into();
+        app.call0(py)
+    })?;
+
+    println!("py: {}", from_python);
+    Ok(())
 }
